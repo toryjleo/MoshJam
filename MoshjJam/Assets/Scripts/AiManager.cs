72be48aa707388e376cc72b3650d7c1e87e3cb59
Tom Perry <Tom.Perry>
2021-03-08 16:31:36 -0500

    I added Ai Manager functionality to spawn ai, set their spawn rate, and spawn otherr such things with '#' will be ignored, and an empty message aborts the commit.

---
+++ MoshjJam/Assets/Scripts/AiManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AiManager : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject Ai_Prefab;
+    private List<Ai_Enemy> AiPool;
+    public int SpawnRate;
+    public int FirstWave; // number of AI starting to spawn 
+    public Vector3 PlayerV;
+    private Vector3 SpawnPoint; 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AiPool = new List<Ai_Enemy>();
+
+        for(int i =0; i < FirstWave; i++)
+        {   
+            //This creates a list of game objects and adds them to the scene,
+            //Then the Ai Componenets are extracted and put into a List called AiPool 
+            GameObject newAI = Instantiate(Ai_Prefab, SpawnPoint, Quaternion.identity);
+            AiPool.Add(newAI.GetComponent<Ai_Enemy>());
+        }
+        
+
+        foreach (Ai_Enemy Ai in AiPool)
+        {
+            Ai.Start(); 
+            Ai.Spawn();
+            
+
+                
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerV = player.transform.position;
+
+        foreach (Ai_Enemy Ai in AiPool)
+        {
+            if (Ai.isAlive == false)
+            {
+                Ai.gameObject.SetActive(false);
+                Debug.Log("AI DEATH");
+            }
+            Ai.GivePlayerLocation(PlayerV);
+            
+ 
+
+        }
+    }
+}
--- MoshjJam/Assets/Scripts/Ai_Enemy.cs
+++ MoshjJam/Assets/Scripts/Ai_Enemy.cs
@@ -5,37 +5,40 @@ using UnityEngine;
 public class Ai_Enemy : PawnScript
 {
 
-    public GameObject player; 
-    public Vector3 VtoPlayer;
+    private Vector3 VtoPlayer;
     public GameObject gun; 
 
-    void Spawn(Vector3 spawnPoint) //Spawns AI at coordinator 
+    public void Spawn() //Spawns AI at coordinator 
     {
-        
+        float spawnY = Random.Range(Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).y, Camera.main.ScreenToWorldPoint(new Vector2(0, Screen.height)).y);
+        float spawnX = Random.Range(Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).x, Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0)).x);
+
+        Vector3 spawnPoint = new Vector3(spawnX, 0, spawnY);
+        transform.position = spawnPoint;
 
     }
 
+    public void GivePlayerLocation(Vector3 P_Location)
+    {
+        VtoPlayer = P_Location - this.transform.position; //This points to the player 
+        VtoPlayer.Normalize(); //Normalizes vector to be relative to 1 
+    }
+
+
+
     // Start is called before the first frame update
     void Start()
-    {
-        //this.Spawn(x, y, z);
+    {    
         movementSpeed = 2;
-
         base.Start();
+
     }
 
     
 
     // Update is called once per frame
     void Update()
-    { 
-
-        Vector3 P_Location = player.transform.position; 
-
-        VtoPlayer = P_Location - this.transform.position; //This points to the player 
-
-        VtoPlayer.Normalize(); //Normalizes vector to be relative to 1 
-
+    {
         transform.Translate(VtoPlayer * movementSpeed * Time.deltaTime); //move twoard player
 
         if (coolDown > 0)
@@ -45,12 +48,9 @@ public class Ai_Enemy : PawnScript
         else
         {
             Shoot(VtoPlayer);
-            coolDown = 150; 
+            coolDown = 150;
             //coolDown = gun.firerate; 
         }
-        
-        
-
 
     }
 }
