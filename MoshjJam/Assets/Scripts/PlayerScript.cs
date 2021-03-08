a75a092ed62db11e8f46f8d658ea791f1c43d9a0
Tory <toryjleo@gmail.com>
2021-03-08 12:59:39 -0500

    switched versions of Unity

--- MoshjJam/Assets/Scripts/PlayerScript.cs
+++ MoshjJam/Assets/Scripts/PlayerScript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerScript : MonoBehaviour
+public class PlayerScript : PawnScript
 {
     // Variables
     public float movementSpeed = 1;
